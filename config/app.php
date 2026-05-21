f2bdabd0b45a99184efbc0a63487c96d37f848de
Luis105AR <lp7377179@gmail.com>
2026-05-21 12:33:03 -0600

    Fix MySQL socket Railway

--- config/app.php
+++ config/app.php
@@ -293,6 +293,7 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'database' => env('DB_DATABASE', 'blog'),
+            'unix_socket' => null,
             'persistent' => false,
             'timezone' => 'UTC',
 
