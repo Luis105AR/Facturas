1b6ad62b0a3d419f06b141e409e925b673c71daf
Luis105AR <lp7377179@gmail.com>
2026-05-21 12:23:44 -0600

    Agregar datasource Railway

--- config/app.php
+++ config/app.php
@@ -288,6 +288,11 @@ return [
         'default' => [
             'className' => Connection::class,
             'driver' => Mysql::class,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'blog'),
             'persistent' => false,
             'timezone' => 'UTC',
 
